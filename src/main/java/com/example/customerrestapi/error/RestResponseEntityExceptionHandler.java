9b2d390737e0fe64d5e8db95a3efd25d5c996b4e
Jackson Barr <jacksonbarr2021@gmail.com>
2022-12-12 00:00:33 -0500

    override MethodArgumentNotValid handler

--- src/main/java/com/example/customerrestapi/error/RestResponseEntityExceptionHandler.java
+++ src/main/java/com/example/customerrestapi/error/RestResponseEntityExceptionHandler.java
@@ -1,23 +1,37 @@
 package com.example.customerrestapi.error;
 
 import com.example.customerrestapi.entity.ErrorMessage;
+import jakarta.annotation.Nullable;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
-@ControllerAdvice
+@RestControllerAdvice
 @ResponseStatus
 public class RestResponseEntityExceptionHandler extends ResponseEntityExceptionHandler {
 
     @ExceptionHandler(CustomerNotFoundException.class)
-    public ResponseEntity<ErrorMessage> customerNotFoundException(CustomerNotFoundException exception, WebRequest request) {
+    public ResponseEntity<ErrorMessage> handleCustomerNotFound(CustomerNotFoundException exception, WebRequest request) {
         ErrorMessage message = new ErrorMessage(HttpStatus.NOT_FOUND, exception.getMessage());
 
         return ResponseEntity.status(HttpStatus.NOT_FOUND)
+                .body(message);
+    }
+
+    @Override
+    protected ResponseEntity<Object> handleMethodArgumentNotValid(
+            MethodArgumentNotValidException ex, HttpHeaders headers, HttpStatusCode status, WebRequest request) {
+        ErrorMessage message = new ErrorMessage(HttpStatus.BAD_REQUEST, ex.getMessage());
+
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST)
                 .body(message);
     }
 }
