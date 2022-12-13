d7e96dc3eaa221a7f8e2d151e8b07984f49c1114
Jackson Barr <jacksonbarr2021@gmail.com>
2022-12-13 04:03:28 -0500

    add StoreController

---
+++ src/main/java/com/example/customerrestapi/controller/StoreController.java
@@ -0,0 +1,42 @@
+package com.example.customerrestapi.controller;
+
+import com.example.customerrestapi.entity.Store;
+import com.example.customerrestapi.error.StoreNotFoundException;
+import com.example.customerrestapi.service.StoreService;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+public class StoreController {
+
+    @Autowired
+    private StoreService storeService;
+
+    @PostMapping("/store")
+    public Store saveStore(@Valid @RequestBody Store store) {
+        return storeService.saveStore(store);
+    }
+
+    @GetMapping("/store/{id:\\d+}")
+    public Store findStoreById(@PathVariable Long id) throws StoreNotFoundException {
+        return storeService.findStoreById(id);
+    }
+
+    @GetMapping("/store")
+    public List<Store> findAllStores() {
+        return storeService.findAllStores();
+    }
+
+    @GetMapping("/store/{name:[a-zA-Z]+}")
+    public Store findStoreByBranchName(@PathVariable String name) throws StoreNotFoundException {
+        return storeService.findStoreByBranchName(name);
+    }
+
+    @DeleteMapping("/store/{id}")
+    public void deleteById(@PathVariable Long id) throws StoreNotFoundException {
+        storeService.deleteStoreById(id);
+    }
+}
