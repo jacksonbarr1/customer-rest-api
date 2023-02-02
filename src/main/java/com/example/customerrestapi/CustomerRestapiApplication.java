95d2cb264e04b7b4a2f2320d47272a4039d9fad5
Jackson Barr <jacksonbarr2021@gmail.com>
2023-02-02 00:42:35 -0500

    define bCryptPasswordEncoder bean

--- src/main/java/com/example/customerrestapi/CustomerRestapiApplication.java
+++ src/main/java/com/example/customerrestapi/CustomerRestapiApplication.java
@@ -2,12 +2,19 @@ package com.example.customerrestapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @SpringBootApplication
 public class CustomerRestapiApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(CustomerRestapiApplication.class, args);
+    }
+
+    @Bean
+    public BCryptPasswordEncoder bCryptPasswordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 
 }
